78cfd4f99443fe893463e8f37bb44e9070fb6b78
LeDangKhoi-Galaxy <kayle4077@gmail.com>
2026-04-13 20:11:47 +0700

    Update FILE.lua

--- FILE.lua
+++ FILE.lua
@@ -1,9 +1,31 @@
-<?xml version='1.0' encoding='UTF-8' standalone='yes' ?>
-<map>
-    <int name="AimAssist_Headshot_Rate" value="95" />
-    <int name="Bullet_Tracking_Smooth" value="1" />
-    <int name="Sensitivity_Level_Max" value="100" />
-    <string name="Touch_Optimization">Samsung_S26_Ultra</string>
-    <boolean name="Send_Crash_Report" value="false" />
-    <boolean name="Enable_Anti_Cheat_Scan" value="false" />
-</map>
+-- GALAXY Premium v4.6 - EXTERNAL PATCH
+-- Author: Le Dang Khoi
+-- Status: OBB Side-by-Side
+
+function OnAimLock()
+    -- 1. CHỈ KÍCH HOẠT KHI NHẤN BẮN
+    if GetPlayerState(FIRE_BUTTON) == 1 then
+        local enemy = GetClosestEnemy()
+        
+        -- 2. CHẾ ĐỘ GHIM (KHÔNG GHIM ĐỊCH GỤC)
+        if enemy and IsAlive(enemy) and GetHealth(enemy) > 0 then
+            -- Ép tọa độ vào xương đầu (Bone 0)
+            SetLockBone(enemy, 0)
+            SetAimSensitivity(100)
+            
+            -- 3. ĐẠN THẲNG (NO SPREAD)
+            SetBulletSpread(0.0)
+        else
+            -- Địch gục hoặc không có địch -> Trả tâm ngay
+            ResetAim()
+        end
+    else
+        ResetAim()
+    end
+end
+
+-- Vòng lặp quét liên tục để bám mục tiêu di động
+while true do
+    OnAimLock()
+    Sleep(10) -- Tối ưu cho màn hình 120Hz của S26 Ultra
+end
